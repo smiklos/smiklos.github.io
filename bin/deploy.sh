1ab2318f61e9faff9bd72f7bebde536e5d0f7258
smiklos <szotsmiklos@gmail.com>
2020-03-27 15:43:04 +0100

    more fixes

---
+++ bin/deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# Run this script to deploy the app to Github Pages.
+
+# Exit if any subcommand fails.
+set -e
+
+echo "Started deploying"
+
+# Checkout gh-pages branch.
+git checkout -b gh-pages
+
+# Build site.
+
+bundle exec jekyll build
+
+# Delete and move files.
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+# Push to master.
+git add -fA
+git commit --allow-empty -m "$(git log -1 --pretty=%B) [ci skip]"
+git push -f -q origin gh-pages
+
+# Move back to previous branch.
+git checkout -
+yarn install --modules-folder ./_assets/yarn
+
+echo "Deployed Successfully!"
+
+exit 0
